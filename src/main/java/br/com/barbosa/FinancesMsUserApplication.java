774a9ec47de3268e6ae45142362676ec5282ae0f
GiselleBarbosa <g88barbosa@gmail.com>
2024-12-29 13:48:56 -0300

    initial commit

---
+++ src/main/java/br/com/barbosa/FinancesMsUserApplication.java
@@ -0,0 +1,13 @@
+package br.com.barbosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesMsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancesMsUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/barbosa/FinancesMsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.barbosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesMsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
