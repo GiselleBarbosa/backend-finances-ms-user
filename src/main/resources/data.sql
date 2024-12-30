c3bb44574fb09f2286d19bf3697889f3ae6b83db
GiselleBarbosa <g88barbosa@gmail.com>
2024-12-30 00:38:18 -0300

    feat(database): adiciona Flyway e Migrations

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT IGNORE INTO tb_user (name, email, password) VALUES ('Giselle', 'giselle@email.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
-
-       INSERT IGNORE INTO tb_role (role_name) VALUES ('ROLE_USER');
-INSERT IGNORE INTO tb_role (role_name) VALUES ('ROLE_ADMIN');
-
-INSERT IGNORE INTO tb_user_role (user_id, role_id) VALUES (1, 1);
-INSERT IGNORE INTO tb_user_role (user_id, role_id) VALUES (1, 2);
