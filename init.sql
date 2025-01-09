8693006406931e7e1f33810b28bbbdf845b82b3f
GiselleBarbosa <g88barbosa@gmail.com>
2025-01-09 08:22:38 -0300

    feat(dto): cria DTO para request de update e adiciona novos campos - criadoEm - atualizadoEm

--- init.sql
+++ init.sql
@@ -10,7 +10,7 @@ CREATE TABLE tb_users
 (
     id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name     VARCHAR(50)  NOT NULL,
-    email    VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
---
+++ src/main/java/br/com/barbosa/dtos/UpdateUserRequestDTO.java
@@ -0,0 +1,27 @@
+package br.com.barbosa.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+public record UpdateUserRequestDTO(
+        @NotBlank(message = "O nome é obrigatório")
+        @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres")
+        @Pattern(regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$", message = "O nome deve conter apenas letras e espaços")
+        String name,
+
+        @NotBlank(message = "O email é obrigatório")
+        @Email(message = "O email deve ser válido")
+        String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        String password,
+
+        @LastModifiedDate()
+        LocalDateTime atualizado_em
+) {
+}
